fec8eaa209aed8968e7aa0ace750444455ea9e5b
DisabledMallis <mallisiscool@gmail.com>
2020-06-04 09:03:05 -0400

    Add minhook & reclass project

---
+++ NKHook5/NKHook5/NKH/BloonSDK.h
@@ -0,0 +1,191 @@
+#pragma once
+// Created with ReClass.NET 1.2 by KN4CK3R
+
+class CBloonsTD5Game
+{
+public:
+	char pad_0000[12]; //0x0000
+	class CTextureLoader* CTextureLoader; //0x000C
+	class WinInput* WinInput; //0x0010
+	class CBloonsTD5Game* Self; //0x0014
+	char pad_0018[4]; //0x0018
+	class WinFileIO* WinFileIO; //0x001C
+	class CScreenManager* CScreenManager; //0x0020
+	class WinSoundManager* WinSoundManager; //0x0024
+	class WinMusicManager* WinMusicManager; //0x0028
+	char pad_002C[4]; //0x002C
+	class SteamStoreInterface* SteamStoreInterface; //0x0030
+	class CSteamInterface* CSteamInterface; //0x0034
+	char pad_0038[4]; //0x0038
+	class CLicensing* CLicensing; //0x003C
+	char pad_0040[16]; //0x0040
+	class CFacebookInterface* CFacebookInterface; //0x0050
+	class CEveryplayInterface* CEveryplayInterface; //0x0054
+	class CCurlHttpRequestManager* CCurlHttpRequestManager; //0x0058
+	char pad_005C[8]; //0x005C
+	class CLoc* CLoc; //0x0064
+	char pad_0068[16]; //0x0068
+	class CMemoryTracker* CMemoryTracker; //0x0078
+	class CTimerTracker* CTimerTracker; //0x007C
+	class CTimerTrackerGraph* CTimerTrackerGraph; //0x0080
+	char pad_0084[192]; //0x0084
+	class CAssetBag* CAssetBag; //0x0144
+	char pad_0148[4]; //0x0148
+	class CGameSystemPointers* CGameSystemPointers; //0x014C
+	char pad_0150[4]; //0x0150
+	class CFont* CFont; //0x0154
+	char pad_0158[4]; //0x0158
+	class CTextObject* CTextObject; //0x015C
+	class CTextObject* N0000030D; //0x0160
+	char pad_0164[212]; //0x0164
+}; //Size: 0x0238
+
+class CTextureLoader
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class WinInput
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class N000000CF
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class WinFileIO
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CScreenManager
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class WinSoundManager
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class WinMusicManager
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class N0000016A
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class N00000190
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class N00000194
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class SteamStoreInterface
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CSteamInterface
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CLicensing
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CFacebookInterface
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CEveryplayInterface
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CCurlHttpRequestManager
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CLoc
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CMemoryTracker
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CTimerTracker
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CTimerTrackerGraph
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CAssetBag
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CGameSystemPointers
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CFont
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class CTextObject
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
+
+class N00000605
+{
+public:
+	char pad_0000[4]; //0x0000
+}; //Size: 0x0004
--- NKHook5/NKHook5/NKH/Hooks.cpp
+++ NKHook5/NKHook5/NKH/Hooks.cpp
@@ -5,12 +5,21 @@
 #include <iostream>
 #include <functional>
 
+#include <MinHook.h>
+
 using namespace std;
+
+//typedef string (__thiscall* FlagToString)(void);
+
 Hooks::Hooks()
 {
-    cout << "Size:" << hex << Utils::getBaseModuleSize() << endl;
-    cout << "Beginning:" << hex << Utils::getModuleBase() << endl;
-    cout << "End:" << hex << Utils::getBaseModuleEnd() << endl;
-    int result = Utils::findPattern(Utils::getModuleBase(), Utils::getBaseModuleEnd(), "56 57 ff 75 0C C7 45 FC 00 00 00 00 E8 E9")-6;
-    cout << "FlagToString:" << hex << result << endl;
+    /*if (MH_Initialize() == MH_OK) {
+        int result = Utils::findPattern(Utils::getModuleBase(), Utils::getBaseModuleEnd(), "56 57 ff 75 0C C7 45 FC 00 00 00 00 E8 E9") - 6;
+        cout << "FlagToString:" << hex << result << endl;
+        MH_CreateHook(result, )
+    }
+    else {
+        cout << "MinHook failed to load!";
+    }*/
+    
 }
