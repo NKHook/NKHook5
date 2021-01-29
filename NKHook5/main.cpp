22ffd35d8e9ac404c6d6e4cf2d43825e7dca8cdc
DisabledMallis <mallisiscool@gmail.com>
2021-01-29 01:06:01 -0500

    Load code before game :sunglasses:

---
+++ NKHook5/main.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+extern "C" __declspec(dllexport) bool InternetGetConnectedState(LPDWORD lpdwFlags, DWORD   dwReserved) {
+    return true;
+}
+
+auto initialize() -> int {
+    AllocConsole();
+    FILE *fDummy;
+    freopen_s(&fDummy, "CONIN$", "r", stdin);
+    freopen_s(&fDummy, "CONOUT$", "w", stderr);
+    freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    std::cout << "Haha doing shit before game go brrrr" << std::endl;
+    Sleep(5000);
+    return 0;
+}
+
+
+extern "C" __declspec(dllexport) bool __stdcall DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved )  // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch( fdwReason ) 
+    { 
+        case DLL_PROCESS_ATTACH:
+            CreateThread(nullptr, (size_t)nullptr, (LPTHREAD_START_ROUTINE)initialize, nullptr, 0, nullptr);
+            break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
