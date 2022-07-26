f25fb74ab98d5f69c1fc99e8dc1d8a8e4b340454
Mike <mallisiscool@gmail.com>
2022-07-26 02:11:26 -0400

    interacting with imgui for some reason causes crashes, but whatever the menu is there. goodnight.

--- NKHook5/MenuEditor/Editor.cpp
+++ NKHook5/MenuEditor/Editor.cpp
@@ -14,9 +14,15 @@ extern Classes::CBloonsTD5Game* g_appPtr;
 void ElementEditor(Classes::CBasePositionableObject* object) {
 	ImGui::PushID(object);
 	if (ImGui::TreeNode("Object")) {
-		ImGui::InputFloat("X", &object->location.x, 1, 10);
-		ImGui::InputFloat("Y", &object->location.y, 1, 10);
-		ImGui::InputFloat("Z", &object->location.z, 1, 10);
+		char addrBuf[64];
+		sprintf_s(addrBuf, 64, "%p", object);
+		bool needsUpdate = ImGui::InputText("Address", addrBuf, 64);
+		needsUpdate |= ImGui::SliderFloat("X", &object->location.x, -1000, 1000);
+		needsUpdate |= ImGui::SliderFloat("Y", &object->location.y, -1000, 1000);
+		needsUpdate |= ImGui::SliderFloat("Z", &object->location.z, -1000, 1000);
+		if (needsUpdate) {
+			object->SetXYZ(object->location);
+		}
 		ImGui::TreePop();
 	}
 	ImGui::PopID();
@@ -36,6 +42,8 @@ void ScreenTree(Classes::CBaseScreen* screen) {
 }
 
 void Editor::Render() {
+	ImGui::ShowDemoWindow();
+
 	ImGui::Begin("UI Inspector");
 	Classes::CScreenManager* screenManager = g_appPtr->basePointers.pCScreenManager;
 	if (screenManager) {
