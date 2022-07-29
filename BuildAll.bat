6638da222b402af5c438ea3d09a4870c067e7f5d
Mike <mallisiscool@gmail.com>
2022-07-28 22:24:46 -0400

    Update BuildAll.bat

--- BuildAll.bat
+++ BuildAll.bat
@@ -3,3 +3,15 @@ cd Build
 cmake .. -A Win32
 cmake --build . --config Debug
 cmake --build . --config Release
+
+timeout 5 > NUL
+
+mkdir Artifacts
+cd Artifacts
+mkdir Debug
+mkdir Release
+cd ..
+copy "NKHook5\Debug\NKHook5.dll" "Artifacts\Debug\NKHook5.dll"
+copy "Loader\Debug\wininet.dll" "Artifacts\Debug\wininet.dll"
+copy "NKHook5\Release\NKHook5.dll" "Artifacts\Release\NKHook5.dll"
+copy "Loader\Release\wininet.dll" "Artifacts\Release\wininet.dll"
