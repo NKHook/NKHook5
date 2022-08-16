7f3f9cc794aa747df91bbb37f3aed0953d6b39af
Mike <mallisiscool@gmail.com>
2022-08-16 16:17:13 -0400

    Start Sprite/Documents rewrite

--- Common/Sprites/Documents/FrameInfo.cpp
+++
@@ -1,8 +0,0 @@
-#include "FrameInfo.h"
-
-using namespace Common::Sprites::Documents;
-
-FrameInfo::FrameInfo(std::string name, TexType type, size_t texw, size_t texh) : TexInfo(name, type) {
-	this->texw = texw;
-	this->texh = texh;
-}
--- Common/Sprites/Documents/XmlInfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "XmlInfo.h"
-
-using namespace Common;
-using namespace Common::Sprites;
-using namespace Common::Sprites::Documents;
-
-XmlInfo::XmlInfo(std::string name, TexType type, bool required) : TexInfo(name, type) {
-	this->required = required;
-}
