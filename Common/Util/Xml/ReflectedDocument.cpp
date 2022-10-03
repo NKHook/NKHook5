b857b8f6b01dc8f91dd145da269570752d30a440
Mike <mallisiscool@gmail.com>
2022-10-03 10:47:07 -0400

    Update ReflectedDocument.cpp

--- Common/Util/Xml/ReflectedDocument.cpp
+++ Common/Util/Xml/ReflectedDocument.cpp
@@ -25,6 +25,76 @@ ReflectedDocument::~ReflectedDocument()
 	delete this->reflect_document;
 }
 
+//A function to do the reflection work recursively
+void ExecuteReflect(rapidxml::xml_node<>* reflectNode, rapidxml::xml_document<>* victim, rapidxml::xml_node<>* current_selection) {
+	//Iterate the child nodes of operations
+	rapidxml::xml_node<>* curOp = reflectNode->first_node();
+	while (curOp) {
+		//Get the operation name
+		std::string opName(curOp->name());
+		//If select operation
+		if (opName == "Select") {
+#pragma region select_operation
+			//Storage for parameters
+			std::string node;
+			std::string match;
+			std::string with;
+
+			//Find the parameters from the attributes
+			rapidxml::xml_attribute<>* opParam = curOp->first_attribute();
+			while (opParam) {
+				std::string paramName(opParam->name());
+
+				if (paramName == "node") {
+					node = opParam->value();
+				}
+				if (paramName == "match") {
+					match = opParam->value();
+				}
+				if (paramName == "with") {
+					with = opParam->value();
+				}
+			}
+
+			//"with" requires a corresponding match
+			if (match.empty() && !with.empty() || !match.empty() && with.empty()) {
+				Print("\"match\" requires a corresponding \"with\" and vice-versa");
+				match = "";
+				with = "";
+			}
+
+			rapidxml::xml_node<>* victimNode = victim->first_node();
+			while (victimNode) {
+				std::string victimNodeName(victimNode->name());
+
+				bool matches = match.empty();
+				if (!match.empty()) {
+					rapidxml::xml_attribute<>* victimAttrib = victimNode->first_attribute();
+					while (victimAttrib) {
+						std::string attribName(victimAttrib->name());
+						std::string attribValue(victimAttrib->value());
+
+						if (attribName == match && attribValue == with) {
+							matches = true;
+						}
+
+						victimAttrib = victimAttrib->next_attribute();
+					}
+				}
+
+				if (victimNodeName == node && matches) {
+					ExecuteReflect(curOp, victim, victimNode);
+				}
+
+				victimNode = victimNode->next_sibling();
+			}
+#pragma endregion
+		}
+
+		curOp = curOp->next_sibling();
+	}
+}
+
 void ReflectedDocument::Reflect(rapidxml::xml_document<>* victim)
 {
 	//Find our reflection node
@@ -33,56 +103,7 @@ void ReflectedDocument::Reflect(rapidxml::xml_document<>* victim)
 		std::string reflectNodeName(reflectNode->name());
 		//Once found, we can start reflection work
 		if (reflectNodeName == "Reflect") {
-			//A place to store the results from selects
-			std::deque<rapidxml::xml_node<>*> selects;
-			//Iterate the child nodes of operations
-			rapidxml::xml_node<>* curOp = reflectNode->first_node();
-			while (curOp) {
-				//Get the operation name
-				std::string opName(curOp->name());
-				//If select operation
-				if (opName == "Select") {
-					//Storage for parameters
-					std::string node;
-					std::string match;
-					std::string with;
-
-					//Find the parameters from the attributes
-					rapidxml::xml_attribute<>* opParam = curOp->first_attribute();
-					while (opParam) {
-						std::string paramName(opParam->name());
-
-						if (paramName == "node") {
-							node = opParam->value();
-						}
-						if (paramName == "match") {
-							match = opParam->value();
-						}
-						if (paramName == "with") {
-							with = opParam->value();
-						}
-					}
-
-					//"with" requires a corresponding match
-					if (match.empty() && !with.empty() || !match.empty() && with.empty()) {
-						Print("\"match\" requires a corresponding \"with\" and vice-versa");
-						match = "";
-						with = "";
-					}
-
-					rapidxml::xml_node<>* victimNode = victim->first_node();
-					while (victimNode) {
-						std::string victimNodeName(victimNode->name());
-						if (victimNodeName == node) {
-							selects.push_back(victimNode);
-						}
-
-						victimNode = victimNode->next_sibling();
-					}
-				}
-
-				curOp = curOp->next_sibling();
-			}
+			ExecuteReflect(reflectNode, victim, nullptr);
 
 			//Return once we're done
 			break;
