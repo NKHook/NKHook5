6bcac69de5b76bf2bed49e5db600841bfb42ccb2
Mike <mallisiscool@gmail.com>
2025-10-24 11:50:44 -0400

    Remove debug messages + bump version number

--- NKHook5/Patches/C_GameSFXManager/Play.cpp
+++ NKHook5/Patches/C_GameSFXManager/Play.cpp
@@ -25,13 +25,11 @@ namespace NKHook5
 				auto* pInstance = reinterpret_cast<Classes::C_GameSFXManager*>(this);
 				int16_t audio = *reinterpret_cast<int16_t*>(&audioId);
 
-				Logger::Print("Playing SFX: %hd (%d) (%p)", audio, audioId, audioId);
                 // Find the correct audio to play
 				for (int i = 0; i < pInstance->mSounds.size(); i++) {
 					auto& sound = pInstance->mSounds.at(i);
 					auto hash = static_cast<Classes::eSFX_Items>(std::hash<nfw::string>()(sound.mName));
 					hash = static_cast<int16_t>(hash) < 0 ? hash : static_cast<Classes::eSFX_Items>(static_cast<int16_t>(hash) * -1);
-					Logger::Print("Testing sound: %s (h: %hd)", sound.mName.c_str(), static_cast<int16_t>(hash));
 					if (audio == static_cast<int16_t>(hash)) {
 						Logger::Print("Found audio hash: %d -> %d", audio, i);
 						auto ofn = std::bit_cast<decltype(&Play::cb_hook)>(reinterpret_cast<void*>(o_func));
